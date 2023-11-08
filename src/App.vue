5344b2c4b8a19c3b19a9233fce0ee0c3d17c9899
Mr.Alien <beidaojiaze@163.com>
2023-11-08 17:46:30 +0800

    路由配置中ing

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,8 @@
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView /> -->
+ <!-- 一级路由出口组件 -->
+  <RouterView /> 
 
   <el-button type="primary">Primary</el-button>
 </template>
